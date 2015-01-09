b6ebd9df088bf419439fe019d1ac6f60dcabafb9
Pía Gómez <pia_gomez_q@hotmail.com>
2015-01-09 00:44:44 -0300

    Proyecto practica mejorandola
    avance de las clases de 1 a 4

---
+++ js/formulario.js
@@ -0,0 +1,26 @@
+var $form = $('#formulario'),
+	$titulo = $('#titulo'),
+	$url = $('#url'),
+	$button = $('#mostrar-form'),
+	$list = $('#contenido'),
+	$post = $('.item').first(); //toma el primer elemento
+
+function mostrarFormulario () {
+	$form.slideToggle();
+	return false;
+}
+function agregarPost(){
+	var url =$url.val(),
+		titulo = $titulo.val(),
+		$clone = $post.clone();
+	$clone.find('.titulo_item a')
+		.text(titulo)
+		.attr('href',url);
+	$clone.hide();
+	$list.prepend($clone);
+	$clone.fadeIn();
+	return false;
+}
+// Eventos
+$button.click(mostrarFormulario);
+$form.on('submit', agregarPost);
