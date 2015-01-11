618db6a5cbdaede3a4113ff6e56f005e8bfeef1b
Pía Gómez <pia_gomez_q@hotmail.com>
2015-01-11 13:57:11 -0300

    clase 5 mejorandola

--- js/formulario.js
+++ js/formulario.js
@@ -5,22 +5,45 @@ var $form = $('#formulario'),
 	$list = $('#contenido'),
 	$post = $('.item').first(); //toma el primer elemento
 
-function mostrarFormulario () {
+if(localStorage.getItem('autosave')){
+	$titulo.val(sessionStorage.getItem('titulo'));
+	$url.val(sessionStorage.getItem('url'));
+}
+
+var id = setInterval(function(){
+	sessionStorage.setItem('titulo', $titulo.val());
+	sessionStorage.setItem('url', $url.val());
+},1000);
+
+function mostrarFormulario (e) {
+	'user strict' //exige escribir las sintaxys correctamente
+	 e.preventDefault(); //manera de reemplazar return false;
+	e.stopPropagation();// detiene los eventos propagados entre las capas ej Nav -> ul -> li -> a click
+
 	$form.slideToggle();
-	return false;
+	$list.slideToggle();
+	// return false;
 }
 function agregarPost(){
 	var url =$url.val(),
 		titulo = $titulo.val(),
-		$clone = $post.clone();
-	$clone.find('.titulo_item a')
+		clone = $post.clone();
+	clone.find('.titulo_item a')
 		.text(titulo)
 		.attr('href',url);
-	$clone.hide();
-	$list.prepend($clone);
-	$clone.fadeIn();
+
+	clone.hide();
+
+	$list.prepend(clone);
+	mostrarFormulario();
+	$titulo.val(''); //entrega valor vacío
+	clone.slideDown();
+	// $clone.fadeIn();
 	return false;
 }
 // Eventos
-$button.click(mostrarFormulario);
-$form.on('submit', agregarPost);
+$('#publicar_nav a').click(mostrarFormulario);
+$('#formulario').on('submit', agregarPost);
+
+// $button.click(mostrarFormulario);
+// $form.on('submit', agregarPost);
